8c0099b1b3e4d173d8b504815200ec0c66335612
=Eslokaren <=>
2020-11-19 16:28:15 -0400

    Reservation section

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,3 +1,18 @@
 $( document ).ready(function() {
     new WOW().init();
+});
+
+$(function() {
+    var selectedClass = "";
+    $(".filter").click(function(){
+    selectedClass = $(this).attr("data-rel");
+    $("#gallery").fadeTo(100, 0.1);
+    $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+    setTimeout(function() {
+    $("."+selectedClass).fadeIn().addClass('animation');
+    $("#gallery").fadeTo(300, 1);
+    }, 300);
     });
+});
+
+$('.datepicker').pickadate();
